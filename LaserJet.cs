603824b351a8860b1230fb7121206872b637d714
nurazmiaprilia <nurazmi.aprilia@students.amikom.ac.id>
2020-05-08 02:57:44 +0700

    Tugas OOP-Polymorphism

---
+++ LaserJet.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class LaserJet : PrinterWindows
+{
+    public override void Show()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Laserjet display dimension : 12*12");
+    }
+    public override void Print()
+    {
+        Console.WriteLine("Laserjet printer printing....");
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class PrinterWindows
+{
+    public virtual void Show()
+    {
+        
+    }
+    public virtual void Print()
+    {
+        
+    }
+}
